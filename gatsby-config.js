c6721ee9863b10264bd1cdc0eb851cc5b69f99ec
Tan Nguyen <tannd1993@gmail.com>
2019-04-21 15:36:07 +0700

    Changes icon and upgrades gatsby-plugin-manifest package

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,31 +19,24 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: 'assets',
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
@@ -55,19 +48,19 @@ module.exports = {
         trackingId: 'UA-62536885-4',
       },
     },
-    `gatsby-plugin-feed`,
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'ngtan',
-    //     short_name: 'ngtan',
-    //     start_url: '/',
-    //     background_color: '#ffffff',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'content/assets/gatsby-icon.png',
-    //   },
-    // },
+    'gatsby-plugin-feed',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'ngtan',
+        short_name: 'ngtan',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#f0db4f',
+        display: 'minimal-ui',
+        icon: 'src/assets/icon.png',
+      },
+    },
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     {
